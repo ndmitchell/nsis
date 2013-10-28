b95d9dd722c852cf9a62574fac00b25f350580df
Neil Mitchell <ndmitchell@gmail.com>
2013-10-28 20:56:53 +0000

    Make the showing have access to the defined functions, useful to wire up page events

--- Development/NSIS/Show.hs
+++ Development/NSIS/Show.hs
@@ -12,12 +12,12 @@ showNSIS :: [NSIS] -> [String]
 showNSIS xs =
     ["!Include MUI2.nsh"] ++
     ["Var _" ++ show v | v <- sort $ nub [i | Var i <- universeBi xs]] ++
-    outs (filter isGlobal xs) ++
+    outs funs (filter isGlobal xs) ++
     ["!insertmacro MUI_LANGUAGE \"English\""] ++
-    outs (filter isSection xs) ++
-    concat [("Function " ++ show name) : map indent (outs body) ++ ["FunctionEnd"] | Function name body <- universeBi xs] ++
+    outs funs (filter isSection xs) ++
+    concat [("Function " ++ show name) : map indent (outs funs body) ++ ["FunctionEnd"] | Function name body <- universeBi xs] ++
     ["Function .onInit" | not $ null inits] ++
-    map indent (outs inits) ++
+    map indent (outs funs inits) ++
     ["FunctionEnd" | not $ null inits] ++
     (if null descs then [] else
         ["!insertmacro MUI_FUNCTION_DESCRIPTION_BEGIN"] ++
@@ -25,6 +25,7 @@ showNSIS xs =
         ["!insertmacro MUI_FUNCTION_DESCRIPTION_END"])
     where descs = filter (not . null . snd) $ concatMap secDescs $ universeBi xs
           inits = filter (\x -> not (isSection x) && not (isGlobal x)) xs
+          funs = [name | Function name body <- universeBi xs]
 
 
 secDescs :: NSIS -> [(SectionId, Val)]
@@ -60,43 +61,43 @@ isSection x = case x of
     _ -> False
 
 
-outs :: [NSIS] -> [String]
-outs = concatMap out
+outs :: [Fun] -> [NSIS] -> [String]
+outs fs = concatMap (out fs)
 
-out :: NSIS -> [String]
-out (Assign v x) = ["StrCpy " ++ show v ++ " " ++ show x]
-out (SetCompressor ACompressor{..}) = [unwords $ "SetCompressor" : ["/solid"|compSolid] ++ ["/final"|compFinal] ++ [map toLower $ show compType]]
-out (Section ASection{secId=SectionId secId, ..} xs) =
+out :: [Fun] -> NSIS -> [String]
+out fs (Assign v x) = ["StrCpy " ++ show v ++ " " ++ show x]
+out fs (SetCompressor ACompressor{..}) = [unwords $ "SetCompressor" : ["/solid"|compSolid] ++ ["/final"|compFinal] ++ [map toLower $ show compType]]
+out fs (Section ASection{secId=SectionId secId, ..} xs) =
     [unwords $ "Section" : ["/o"|secUnselected] ++ [show $ [Literal "!"|secBold] ++ secName, "_sec" ++ show secId]] ++
-    map indent (["SectionIn RO" | secRequired] ++ outs xs) ++
+    map indent (["SectionIn RO" | secRequired] ++ outs fs xs) ++
     ["SectionEnd"]
-out (SectionGroup ASectionGroup{secgId=SectionId secgId, ..} xs) =
+out fs (SectionGroup ASectionGroup{secgId=SectionId secgId, ..} xs) =
     [unwords $ "SectionGroup" : ["/e"|secgExpanded] ++ [show secgName, "_sec" ++ show secgId]] ++
-    map indent (outs xs) ++
+    map indent (outs fs xs) ++
     ["SectionGroupEnd"]
-out (File AFile{..}) = [unwords $ "File" : ["/nonfatal"|fileNonFatal] ++ ["/r"|fileRecursive] ++ [show filePath]]
-out (Labeled i) = [show i ++ ":"]
-out (CreateShortcut AShortcut{..}) = return $ unwords $
+out fs (File AFile{..}) = [unwords $ "File" : ["/nonfatal"|fileNonFatal] ++ ["/r"|fileRecursive] ++ [show filePath]]
+out fs (Labeled i) = [show i ++ ":"]
+out fs (CreateShortcut AShortcut{..}) = return $ unwords $
     ["CreateShortcut", show scFile, show scTarget, show scParameters, show scIconFile
     ,show scIconIndex, show scStartOptions, show scKeyboardShortcut, show scDescription]
-out (InstallIcon x) = ["!define MUI_ICON " ++ show x]
-out (UninstallIcon x) = ["!define MUI_UNICON " ++ show x]
-out (HeaderImage x) = "!define MUI_HEADERIMAGE" : ["!define MUI_HEADERIMAGE_BITMAP " ++ show x | Just x <- [x]]
-out (Page x) = ["!insertmacro MUI_PAGE_" ++ showPage x]
-out (Unpage x) = ["!insertmacro MUI_UNPAGE_" ++ showPage x]
-out Function{} = []
-out (Delete ADelete{..}) = [unwords $ "Delete" : ["/rebootok"|delRebootOK] ++ [show delFile]]
-out (RMDir ARMDir{..}) = [unwords $ "RMDir" : ["/r"|rmRecursive] ++ ["/rebootok"|rmRebootOK] ++ [show rmDir]]
-out (CopyFiles ACopyFiles{..}) = [unwords $ "CopyFiles" : ["/silent"|cpSilent] ++ ["/filesonly"|cpFilesOnly] ++ [show cpFrom, show cpTo]]
-out (MessageBox flags txt lbls) = [unwords $ "MessageBox" : intercalate "|" (map show flags) : show txt :
+out fs (InstallIcon x) = ["!define MUI_ICON " ++ show x]
+out fs (UninstallIcon x) = ["!define MUI_UNICON " ++ show x]
+out fs (HeaderImage x) = "!define MUI_HEADERIMAGE" : ["!define MUI_HEADERIMAGE_BITMAP " ++ show x | Just x <- [x]]
+out fs (Page x) = ["!insertmacro MUI_PAGE_" ++ showPage x]
+out fs (Unpage x) = ["!insertmacro MUI_UNPAGE_" ++ showPage x]
+out fs Function{} = []
+out fs (Delete ADelete{..}) = [unwords $ "Delete" : ["/rebootok"|delRebootOK] ++ [show delFile]]
+out fs (RMDir ARMDir{..}) = [unwords $ "RMDir" : ["/r"|rmRecursive] ++ ["/rebootok"|rmRebootOK] ++ [show rmDir]]
+out fs (CopyFiles ACopyFiles{..}) = [unwords $ "CopyFiles" : ["/silent"|cpSilent] ++ ["/filesonly"|cpFilesOnly] ++ [show cpFrom, show cpTo]]
+out fs (MessageBox flags txt lbls) = [unwords $ "MessageBox" : intercalate "|" (map show flags) : show txt :
     ["ID" ++ a ++ " " ++ show b | (a,b) <- lbls]]
-out (Goto x) = ["Goto " ++ show x | x /= Label 0]
-out (IntOp a b "~" _) = [unwords $ "IntOp" : [show a, show b, "~"]] -- the only unary IntOp
-out (ExecShell AExecShell{..}) = [unwords ["ExecShell","\"\"",show esCommand,show esShow]]
-out (Plugin a b cs) = [unwords $ (a ++ "::" ++ b) : map show cs]
-out (AddPluginDir a) = [unwords ["!addplugindir",show a]]
+out fs (Goto x) = ["Goto " ++ show x | x /= Label 0]
+out fs (IntOp a b "~" _) = [unwords $ "IntOp" : [show a, show b, "~"]] -- the only unary IntOp
+out fs (ExecShell AExecShell{..}) = [unwords ["ExecShell","\"\"",show esCommand,show esShow]]
+out fs (Plugin a b cs) = [unwords $ (a ++ "::" ++ b) : map show cs]
+out fs (AddPluginDir a) = [unwords ["!addplugindir",show a]]
 
-out x = [show x]
+out fs x = [show x]
 
 
 showPage :: Page -> String
