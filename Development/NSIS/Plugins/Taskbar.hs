01311175e489e60b4cffe38dfcec9b1f55b56b9d
Neil Mitchell <ndmitchell@gmail.com>
2013-10-28 21:15:26 +0000

    Add a Win7 taskbar progress plugin module

---
+++ Development/NSIS/Plugins/Taskbar.hs
@@ -0,0 +1,10 @@
+
+-- | Windows 7 Taskbar Progress plugin: <http://nsis.sourceforge.net/TaskbarProgress_plug-in>
+module Development.NSIS.Plugins.Taskbar(taskbar) where
+
+import Development.NSIS
+
+
+-- | Enable Windows 7 taskbar plugin, called anywhere.
+taskbar :: Action ()
+taskbar = event "ShowInstFiles" $ plugin "w7tbp" "Start" []
