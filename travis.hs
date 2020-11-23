5635162d2d9179012c83e5d2e9d20eb1b1db3468
Neil Mitchell <ndmitchell@gmail.com>
2020-11-23 21:40:52 +0000

    Build on CI on Windows and Linux

--- travis.hs
+++ travis.hs
@@ -1,11 +1,11 @@
 
 import Control.Exception.Extra
 import System.Process.Extra
-import System.Info
+import System.Info.Extra
 import Control.Monad
 
-main = do
-    putStrLn $ "Only running tests on linux, OS = " ++ show os
-    when (os == "linux") $ do
-        retry 3 $ system_ "sudo apt-get install nsis"
+main =
+    unless isMac $ do
+        unless isWindows $
+            retry 3 $ system_ "sudo apt-get install nsis"
         system_ "cabal test --test-option=--build"
