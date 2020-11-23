c182dcbff78de0109800805fd7fc0144f1e09919
Neil Mitchell <ndmitchell@gmail.com>
2020-11-23 21:15:41 +0000

    Only run the tests on Linux

--- travis.hs
+++ travis.hs
@@ -4,7 +4,8 @@ import System.Process.Extra
 import System.Info
 import Control.Monad
 
-main =
+main = do
+    putStrLn $ "Only running tests on linux, OS = " ++ show os
     when (os == "linux") $ do
         retry 3 $ system_ "sudo apt-get install nsis"
         system_ "cabal test --test-option=--build"
