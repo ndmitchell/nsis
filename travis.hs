a376769acd970feec2232a1eb3658e7aeb951ecb
Neil Mitchell <ndmitchell@gmail.com>
2020-11-23 20:42:42 +0000

    Only try install nsis on Linux, since otherwise it won't exist

--- travis.hs
+++ travis.hs
@@ -1,7 +1,10 @@
 
 import Control.Exception.Extra
 import System.Process.Extra
+import System.Info
+import Control.Monad
 
-main = do
-    retry 3 $ system_ "sudo apt-get install nsis"
-    system_ "cabal test --test-option=--build"
+main =
+    when (os == "linux") $ do
+        retry 3 $ system_ "sudo apt-get install nsis"
+        system_ "cabal test --test-option=--build"
